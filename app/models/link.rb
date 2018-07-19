a3af3d094b1d2d789e56326c54177b4a6ec984dd
libovness <jonathan.libov@gmail.com>
2018-07-18 23:07:59 -0400

    added logo

--- app/models/link.rb
+++ app/models/link.rb
@@ -4,6 +4,5 @@ class Link < ApplicationRecord
 	belongs_to :user, optional: true
 	validates :title, presence: true
 	validates :url, presence: true
-	validates_format_of :url, :with => /^(http|https):\/\/|[a-z0-9]+([\-\.]{1}[a-z0-9]+)*\.[a-z]{2,6}(:[0-9]{1,5})?(\/.*)?$/ix
 
 end
