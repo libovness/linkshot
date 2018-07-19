c999cbbed6f62c5d9ff7da442573272d152b706b
libovness <jonathan.libov@gmail.com>
2018-07-18 23:31:57 -0400

    trying again with image serving

--- config/application.rb
+++ config/application.rb
@@ -11,6 +11,7 @@ module Linkshot
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.1
     config.assets.paths << Rails.root.join("app", "assets", "fonts")
+    config.assets.paths << Rails.root.join("app", "assets", "images")
     config.serve_static_assets = true
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
