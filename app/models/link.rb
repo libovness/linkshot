28730eac5be2e04d26fb7d46acfc5ba94ed6b805
libovness <jonathan.libov@gmail.com>
2018-07-19 23:01:24 -0400

    made links visual

--- app/models/link.rb
+++ app/models/link.rb
@@ -1,27 +1,21 @@
 class Link < ApplicationRecord
 
 	require 'mechanize'
+	before_save :check_for_metadata, if: :url_changed?, on: :update
+	before_save :check_for_metadata, on: :create
 
 	belongs_to :shot, inverse_of: :links, optional: true
 	belongs_to :user, optional: true
 	# validates :title, presence: true
 	validates :url, presence: true
 
-	# answer = self.changed?
-	# puts answer
-
-	def save_metadata 
-		if url_changed?
-			agent = Mechanize.new
-			page = agent.get(url)
-			title = page.title
-			puts title
-			description = page.at("head meta[name='description']").attributes["content"].value unless page.at("head meta[name='description']").nil?
-			puts description
-			image = page.at("head meta[property='og:image']").attributes["content"].value unless page.at("head meta[property='og:image']").nil?
-			puts image
-			self.update_attributes(title: title, description: description, image: image)
-		end
+	def check_for_metadata
+		agent = Mechanize.new
+		page = agent.get(url)
+		agent.keep_alive = false
+		self.title = page.title
+		self.description = page.at("head meta[name='description']").attributes["content"].value unless page.at("head meta[name='description']").nil?
+		self.image = page.at("head meta[property='og:image']").attributes["content"].value unless page.at("head meta[property='og:image']").nil?
 	end
 
 end
